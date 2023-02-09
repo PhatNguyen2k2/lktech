94fc8298b1133bf4a6439b429c7ce4c4af566b25
PhatNguyen2k2 <nguyenphatbe@gmail.com>
2023-02-09 11:29:00 +0700

    add header

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Loading from "./components/Loading/Loading";
+
+const ViewHome = React.lazy(() => import("./pages/ViewHome"));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<ViewHome />} />
+        </Routes>
+      </Suspense>
+    </>
   );
 }
 
---
+++ client/src/pages/ViewHome.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Header from "../components/Header/Header";
+
+const ViewHome = () => {
+  return <Header />;
+};
+export default ViewHome;

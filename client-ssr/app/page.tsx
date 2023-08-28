a23e721a11794739f27ab2fa0686c71617ea8c66
PhatNguyen2k2 <nguyenphatbe@gmail.com>
2023-08-28 20:59:55 +0700

    add layout to main layout

--- client-ssr/app/page.tsx
+++ client-ssr/app/page.tsx
@@ -1,12 +1,10 @@
-import Layout from '@/components/layout'
+import Banner from '@/components/Banner/banner'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <Layout>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 className="text-6xl font-bold text-center">Hello World</h1>
-      </main>
-    </Layout>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Banner />
+    </main>
   )
 }

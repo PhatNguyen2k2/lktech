bca2a0f478a0de765487cbd0c9c49f25416785d6
PhatNguyen2k2 <nguyenphatbe@gmail.com>
2023-09-02 22:24:56 +0700

    add introduce & partnerShow into Home page

--- client-ssr/app/page.tsx
+++ client-ssr/app/page.tsx
@@ -1,10 +1,14 @@
+import React from 'react';
 import Banner from '@/components/Banner/banner'
-import Image from 'next/image'
+import Introduce from '@/components/Introduce/introduce'
+import PartnerShow from '@/components/Partners/partnerShow';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Banner />
+      <Introduce />
+      <PartnerShow />
     </main>
   )
 }

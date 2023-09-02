6df778afe9f3a11672b01068db9aa8593b0fb6bd
PhatNguyen2k2 <nguyenphatbe@gmail.com>
2023-09-02 22:20:01 +0700

    add styles for footer

--- client-ssr/components/Footer/footer.tsx
+++ client-ssr/components/Footer/footer.tsx
@@ -1,20 +1,20 @@
 import { FC } from "react";
 import Link from "next/link";
-
+import styles from "./footer.module.scss";
 const Footer: FC = () => {
   return (
-    <footer className="bg-gray-900 text-white py-10">
-      <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-4">
-        <div className="introduce">
-          <ul className="ulIntro">
+    <footer className={`bg-gray-900 py-10 ${styles.footer}`}>
+      <div className="container place-items-stretch mx-auto grid grid-cols-1 md:grid-cols-3 gap-4">
+        <div className={styles.introduce}>
+          <ul className={styles.ulIntro}>
             <strong className="block mb-3">Công Ty TNHH L&K TECH</strong>
             <li>Đ/c: 149 Nguyễn Thị Định, P.Bình Trưng Tây, Q.2, Tp.Hồ Chí Minh</li>
             <li>ĐT: (+84) 286 286 7101</li>
             <li>Giờ làm việc: (8h00 – 17h00 | Từ thứ 2 đến thứ 7)</li>
           </ul>
         </div>
-        <div className="service">
-          <ul className="ulService">
+        <div className={styles.service}>
+          <ul className={styles.ulService}>
             <strong className="block mb-3">DỊCH VỤ HỖ TRỢ</strong>
             <li>
               <Link href="/maintenance"> Bảo trì</Link>
@@ -27,15 +27,15 @@ const Footer: FC = () => {
             </li>
           </ul>
         </div>
-        <div className="info">
-          <ul className="ulInfo">
+        <div className={styles.info}>
+          <ul className={styles.ulInfo}>
             <strong className="block mb-3">THÔNG TIN CHÚNG TÔI</strong>
             <li>Email: khoa.le@lktech.com.vn</li>
             <li>090 777 3041 (Mr.Khoa) </li>
           </ul>
         </div>
       </div>
-      <p className="text-center mt-8">&copy; 2023 Công Ty TNHH L&K TECH</p>
+      <p className="container text-center mt-8">&copy; 2023 Công Ty TNHH L&K TECH</p>
     </footer>
   );
 };

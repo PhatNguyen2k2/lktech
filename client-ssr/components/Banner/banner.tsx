b927566b0a3eeade59d98414b9961cfdaca4520d
PhatNguyen2k2 <nguyenphatbe@gmail.com>
2023-09-02 22:19:38 +0700

    add styles for banner

--- client-ssr/components/Banner/banner.tsx
+++ client-ssr/components/Banner/banner.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React, { useEffect, useState } from "react";
 import { AnimationOnScroll } from "react-animation-on-scroll";
+import styles from "./banner.module.scss"
 
 const Banner: React.FC = () => {
     const [btnChange, setBtnChange] = useState<number>(1);
@@ -37,99 +38,99 @@ const Banner: React.FC = () => {
     }, [btnChange, count]);
 
     return (
-        <div className="bg-blue-500">
+        <div className={`container ${styles.banner}`}>
             <video
                 autoPlay
                 muted
                 loop
-                className="video"
+                className={styles.video}
                 src="https://res.cloudinary.com/dwpjjpgrz/video/upload/v1676654109/Panel%20intro%20video/banh-rang-56376_cwwcnf.mp4"
             />
             {btnChange === 1 && (
-                <div className="bannerTrans">
+                <div className={styles.bannerTrans}>
                     <img
-                        className="imageLaser"
+                        className={styles.imageLaser}
                         src="https://res.cloudinary.com/dwpjjpgrz/image/upload/v1676049422/Product%20img/image-removebg-preview_rjjnbc.png"
                         alt="Laser"
                     />
 
-                    <div className="bannerDescription">
+                    <div className={styles.bannerDescription}>
                         <AnimationOnScroll
                             initiallyVisible={true}
                             animateIn="animate__backInLeft"
                             duration={0.4}
                         >
-                            <h1>Laser </h1>
-                            <h2>Super laser, bạn của mọi nhà</h2>
+                            <p className={styles.bannerTitle}>Laser</p>
+                            <p className={styles.bannerContent}>Super laser, bạn của mọi nhà</p>
                         </AnimationOnScroll>
                     </div>
-                    <button id="btnOne" type="button">
+                    <button id={styles.btnOne} type="button">
                         Laser
                     </button>
                 </div>
             )}
             {btnChange === 2 && (
-                <div className="bannerTrans">
+                <div className={styles.bannerTrans}>
                     <img
-                        className="imageKhi"
+                        className={styles.imageKhi}
                         src="https://res.cloudinary.com/dwpjjpgrz/image/upload/v1676086379/Product%20img/Kh%C3%AD_vzoz4f.png"
                         alt="khi"
                     />
-                    <div className="bannerDescriptionKhi">
+                    <div className={styles.bannerDescriptionKhi}>
                         <AnimationOnScroll
                             initiallyVisible={true}
                             animateIn="animate__backInRight"
                             duration={0.4}
                         >
-                            <h1>Khí</h1>
-                            <h2>Super khí, bạn của mọi nhà</h2>
+                            <p className={styles.bannerTitle}>Khí</p>
+                            <p className={styles.bannerContent}>Super khí, bạn của mọi nhà</p>
                         </AnimationOnScroll>
                     </div>
-                    <button id="btnTwo" type="button">
+                    <button id={styles.btnTwo} type="button">
                         Khí
                     </button>
                 </div>
             )}
             {btnChange === 3 && (
-                <div className="bannerTrans">
+                <div className={styles.bannerTrans}>
                     <img
-                        className="imageChemical"
+                        className={styles.imageChemical}
                         src="https://res.cloudinary.com/dwpjjpgrz/image/upload/v1676022018/Product%20img/hoachat-removebg-preview_v2mbdk.png"
                         alt="Chemical"
                     />
-                    <div className="bannerDescription">
+                    <div className={styles.bannerDescription}>
                         <AnimationOnScroll
                             initiallyVisible={true}
                             animateIn="animate__backInLeft"
                             duration={0.4}
                         >
-                            <h1>Hóa chất</h1>
-                            <h2>Super hóa chất, bạn của mọi nhà</h2>
+                            <p className={styles.bannerTitle}>Hóa chất</p>
+                            <p className={styles.bannerContent}>Super hóa chất, bạn của mọi nhà</p>
                         </AnimationOnScroll>
                     </div>
-                    <button id="btnThree" type="button">
+                    <button id={styles.btnThree} type="button">
                         Hóa chất
                     </button>
                 </div>
             )}
-            <div className="bannerButton">
+            <div className={styles.bannerButton}>
                 <button
                     type="button"
-                    className="btn"
+                    className={styles.btn}
                     onClick={() => clickHandle("laser")}
                 >
                     Laser
                 </button>
                 <button
                     type="button"
-                    className="btn"
+                    className={styles.btn}
                     onClick={() => clickHandle("khi")}
                 >
                     Khí
                 </button>
                 <button
                     type="button"
-                    className="btn"
+                    className={styles.btn}
                     onClick={() => clickHandle("chemical")}
                 >
                     Hóa chất

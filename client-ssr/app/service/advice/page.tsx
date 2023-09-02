0f8259a0221d2c91dcb359976498b7a08649700f
PhatNguyen2k2 <nguyenphatbe@gmail.com>
2023-09-02 22:24:10 +0700

    add advice/service page route

---
+++ client-ssr/app/service/advice/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Service from "@/components/Service/service";
+
+export default function Advice() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <Service type={"advice"} />
+        </main>
+    );
+};

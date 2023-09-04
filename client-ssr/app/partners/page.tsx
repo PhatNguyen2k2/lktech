ed6a47c1772150b2497284e805698654feafc947
PhatNguyen2k2 <nguyenphatbe@gmail.com>
2023-09-04 17:49:24 +0700

    add partner page

---
+++ client-ssr/app/partners/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Partners from "@/components/Partners/partner";
+export default function Partner() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between">
+            <Partners />
+        </main>
+    );
+};

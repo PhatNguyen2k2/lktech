8d8b2546392ae8677b6d25974b972ebbad4e010d
PhatNguyen2k2 <nguyenphatbe@gmail.com>
2023-09-04 17:44:50 +0700

    fix view for partnerShow

--- client-ssr/components/Partners/partnerShow.tsx
+++ client-ssr/components/Partners/partnerShow.tsx
@@ -14,8 +14,8 @@ const PartnerShow: React.FC = async () => {
     const data = await db.collection('partners').find({}).toArray()
     const partners: Partner[] = JSON.parse(JSON.stringify(data))
     return (
-        <div className="container bg-gray-100">
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5">
+        <div className="mx-auto bg-gray-100">
+            <div className="justify-items-center grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-5 lg:gap-10 sm:p-14 xs:p-5 gap-5">
                 {partners && partners.map((lg, index) => (
                     <a href={lg.link} key={index} className={styles.Pnlogo} target="_blank" rel="noreferrer">
                         <img className={styles.imagelogo} src={lg.logo} alt={lg.name} />

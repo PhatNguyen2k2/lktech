003df454562a9280dfe1c4bde4dae12e4d6258e2
PhatNguyen2k2 <nguyenphatbe@gmail.com>
2023-09-04 17:47:38 +0700

    fix view

--- client-ssr/app/service/advice/page.tsx
+++ client-ssr/app/service/advice/page.tsx
@@ -3,7 +3,7 @@ import Service from "@/components/Service/service";
 
 export default function Advice() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main className="flex min-h-screen flex-col items-center justify-between">
             <Service type={"advice"} />
         </main>
     );
